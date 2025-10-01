e9e54190bfa34dbc5c702d270ce286d18a49d4fc
zerox13 <abdulsalam.aldahir@gmail.com>
2025-10-01 12:27:59 +0200

    Added nvim setup

---
+++ .config/nvim/README.md
@@ -0,0 +1,10 @@
+# nvim
+
+## OBS 
+* Some remaps are made to work on mac. 
+  * leader lv will try to use "open" to launch pdfs
+
+## Install
+* Install NodeJS
+* Might cloned in ˜/.config
+
---
+++ .config/nvim/init.lua
@@ -0,0 +1,11 @@
+require("core.lazy")
+require("core.lsp")
+
+
+require("config.remap")
+require("config.options")
+--require("config.color")
+require("config.settings_functions")
+
+Disable_arrow_keys()
+
