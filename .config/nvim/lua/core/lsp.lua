d61c00a38ce7c5fc6e01de0ea5399ab181583e87
zerox13 <abdulsalam.aldahir@gmail.com>
2025-11-30 03:22:00 +0100

    Added blink

--- .config/nvim/lua/core/lsp.lua
+++ .config/nvim/lua/core/lsp.lua
@@ -11,10 +11,10 @@ vim.api.nvim_create_autocmd('LspAttach', {
         local client = vim.lsp.get_client_by_id(ev.data.client_id)
         if client and client:supports_method(vim.lsp.protocol.Methods.textDocument_completion) then
             vim.opt.completeopt = { 'menu', 'menuone', 'noinsert', 'fuzzy', 'popup' }
-            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+            vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = false })
 
             -- Keymaps
-            vim.keymap.set('i', '<C-Space>', function()
+            vim.keymap.set('i', '<C-n>', function()
                 vim.lsp.completion.get()
             end)
 
--- .config/nvim/lua/plugins/git-stuff.lua
+++ .config/nvim/lua/plugins/git-stuff.lua
@@ -27,6 +27,7 @@ return {
 
             vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
             vim.keymap.set("n", "<leader>gb", ":Gitsigns toggle_current_line_blame<CR>", {})
+            vim.keymap.set("n", "<leader>gr", ":Gitsigns reset_hunk<CR>", {})
         end
 
     },
