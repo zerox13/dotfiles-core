4c1ee47a012867c2e370e13f3c8ba9ce7a96d34b
zerox13 <abdulsalam.aldahir@gmail.com>
2025-12-01 23:59:23 +0100

    Changed the theme and added recording status to lualine

--- .config/nvim/lua/config/theme.lua
+++ .config/nvim/lua/config/theme.lua
@@ -3,7 +3,7 @@
 
 local function themeSetup(color)
     vim.o.background = "dark" -- or "light" for light mode
-    color = color or "gruvbox"
+    color = color or "tokyonight"
     -- color = color or "dracula"
     vim.cmd.colorscheme(color)
 
@@ -11,9 +11,9 @@ local function themeSetup(color)
 
 
     -- Telescope border colors
-    vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg = '#fb4934' }) -- Red
-    vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { fg = '#b8bb26' }) -- Green
-    vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { fg = '#fabd2f' }) -- Yellow
+    -- vim.api.nvim_set_hl(0, 'TelescopePromptBorder', { fg = '#fb4934' }) -- Red
+    -- vim.api.nvim_set_hl(0, 'TelescopeResultsBorder', { fg = '#b8bb26' }) -- Green
+    -- vim.api.nvim_set_hl(0, 'TelescopePreviewBorder', { fg = '#fabd2f' }) -- Yellow
 end
 
 themeSetup()
--- .config/nvim/lua/plugins/color-schemes.lua
+++ .config/nvim/lua/plugins/color-schemes.lua
@@ -1,5 +1,11 @@
-return{
-    { "ellisonleao/gruvbox.nvim", priority = 1000 , config = true, opts = ...},
-    { "Mofiqul/dracula.nvim", priority = 1000 , config = true, opts = ...},
-    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+return {
+    { "ellisonleao/gruvbox.nvim", priority = 1000,     config = true,  opts = ... },
+    { "Mofiqul/dracula.nvim",     priority = 1000,     config = true,  opts = ... },
+    { "catppuccin/nvim",          name = "catppuccin", priority = 1000 },
+    {
+        "folke/tokyonight.nvim",
+        lazy = false,
+        priority = 1000,
+        opts = {},
+    }
 }
