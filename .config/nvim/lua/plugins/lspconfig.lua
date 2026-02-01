7b98f342c3f0e2068ae8a14315e294261aee8815
zerox13 <abdulsalam.aldahir@gmail.com>
2026-02-01 15:23:22 +0100

    chore: lsp and shell config

--- .config/nvim/lua/plugins/lspconfig.lua
+++ .config/nvim/lua/plugins/lspconfig.lua
@@ -1,5 +1,20 @@
 return {
     "neovim/nvim-lspconfig",
+    opts = {
+        servers = {
+            ["*"] = {
+                -- stylua: ignore
+                keys = {
+                    { "gd", function() require("telescope.builtin").lsp_definitions({ reuse_win = true }) end,      desc = "Goto Definition",       has = "definition" },
+                    { "gr", "<cmd>Telescope lsp_references<cr>",                                                    desc = "References",            nowait = true },
+                    { "gI", function() require("telescope.builtin").lsp_implementations({ reuse_win = true }) end,  desc = "Goto Implementation" },
+                    { "gy", function() require("telescope.builtin").lsp_type_definitions({ reuse_win = true }) end, desc = "Goto T[y]pe Definition" },
+                },
+            },
+        },
+    },
+
+
     config = function()
         -- Luaaa
         vim.lsp.enable('lua_ls')
